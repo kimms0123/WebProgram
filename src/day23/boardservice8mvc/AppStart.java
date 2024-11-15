6c28afad6898a2580b38bf7b3b55d79e67e82e28
kmskmk21 <kmskmk21@gmail.com>
2024-11-15 18:48:58 +0900

    day23

---
+++ src/day23/boardservice8mvc/AppStart.java
@@ -0,0 +1,14 @@
+package day23.boardservice8mvc;
+
+import day23.boardservice8mvc.view.BoardView;
+
+public class AppStart {
+    public static void main(String[] args) {
+        // 메인 뷰 요청
+        // - 다른 클래스의 존재하는 함수를 호출하는 방법
+        // -- 1.객체를 생성한 후 객체명.메소드명()
+        // -- 단 패키지가 다르면 public 함수로 선언한다.
+        BoardView view = new BoardView();
+        view.mainPage();
+    } // m end
+}
